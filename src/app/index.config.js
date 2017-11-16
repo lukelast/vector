d499b4cd8887746fd1dc1e89323c7233dd883da7
Luke Last <luke@lukelast.com>
2017-11-16 12:30:08 -0500

    Change interval to 3 seconds and window to 2 minutes because the UI is so slow.

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -36,8 +36,8 @@
             'protocol': 'https', // PMWEBD protocol (http or https)
             'port': 443,  // PMWEBD port
             'hostspec': 'localhost', // Default PMCD hostspec
-            'interval': '2', // Default update interval in seconds
-            'window': '5', // Default graph time window in minutes
+            'interval': '3', // Default update interval in seconds
+            'window': '2', // Default graph time window in minutes
             'enableCpuFlameGraph': false, // Enable CPU flame graph (requires extra PMDA)
             'enableContainerWidgets': true, // Enable container widgets
             'disableHostspecInput': true, // Disable hostspec input
